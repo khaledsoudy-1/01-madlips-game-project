7f0e040d643aa671dcc95119ce0b048fce4e59c7
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-09-18 18:25:33 +0300

    Enhancement: organized code into function and added script entry point.

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 from random_madlips import adventure, football, holiday, love, school, space, travel
 import random
 
-# A function for getting a random madlip
+
+# Function to get a random madlip
 def get_random_madlip():
     random_choice = random.choice([adventure, football, holiday, love, school, space, travel])
     return random_choice.madlip()
 
-while True:
-    # Print a welcome message
-    print("Please, enter the following:\n")
-    
-    # Call the function inside the loop
-    get_random_madlip()
-    
-    # Ask user if they want to play again.
-    print("Play again ?!")
-    
-    playagain = input("Y for Yes\nQ to Quit\n").lower().strip()
-    
-    # Enter the following loop only if wrong input was given.
-    while playagain not in ["y", "q"]:
-        playagain = input("Y for Yes\nQ to Quit\n")
+# Function to play the Madlips game
+def play_madlips():
+    while True:
+        # Print instructions for the user
+        print("Please, enter the following:\n")
         
-    # If user wants to play again. ==> Continue Looping
-    if playagain == 'y':
-        # Print a welcome back message
-        print("\n==== Welcome Back 😍 ====")
-        continue
+        # Display a random madlip
+        get_random_madlip()
+        
+        # Ask user if they want to play again.
+        print("Play again ?!")
+        playagain = input("Y for Yes\nQ to Quit\n").lower().strip()
+        
+        # Enter the following loop only if wrong input was given.
+        while playagain not in ["y", "q"]:
+            playagain = input("Y for Yes\nQ to Quit\n")
+        
+        # Continue the game if user chooses to play again
+        if playagain == 'y':
+            # Print a welcome back message
+            print("\n==== Welcome Back 😍 ====")
+            continue
+        
+        # Exit the game if user chooses to quit
+        if playagain == 'q':
+            print("\n🎉🎉Thank you for playing!\nBye 👋")
+            break
 
-    # If user does not want to play again.
-    if playagain == 'q':
-        print("\n🎉🎉Thank you for playing!\nBye 👋")
-        break
+
+if __name__ == '__main__':
+    print("===== Welcome to The Madlips Game 🤖 =====")
+    play_madlips()
